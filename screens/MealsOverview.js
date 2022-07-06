59354e4d74e2f647d797ff793a43c131c1919730
chebihiF <chebihi.f@ucd.ac.ma>
2022-07-06 21:32:08 +0100

    dislay meals title

--- screens/MealsOverview.js
+++ screens/MealsOverview.js
@@ -1,11 +1,30 @@
 /** @format */
 
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
-const MealsOverview = () => {
+const MealsOverview = ({ route }) => {
+  const categoryId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(categoryId) >= 0;
+  });
+
+  const rederMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={rederMealItem}
+      />
     </View>
   );
 };
