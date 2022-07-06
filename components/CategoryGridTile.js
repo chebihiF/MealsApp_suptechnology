f5f25368d95003263da1e51e54e6f2dd2e4762ab
chebihiF <chebihi.f@ucd.ac.ma>
2022-07-06 19:42:46 +0100

    add categoriesScreen

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import { Pressable, View } from "react-native";
+
+const CategoryGridTile = ({ title, color }) => {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
